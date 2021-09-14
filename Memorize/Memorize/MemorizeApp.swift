e685710445db68f3335540bd49006788d22e7d8b
Seunghwanly <seunghwanly@gmail.com>
2021-09-14 13:30:27 +0900

    🤩 init Memorize project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 이승환 on 2021/09/14.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
